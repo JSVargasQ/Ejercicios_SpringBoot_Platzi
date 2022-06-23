faea6d144fd0132296a9f512324cc0321b1054b7
JSVargasQ <42383191+Sagrav10@users.noreply.github.com>
2022-06-22 21:50:22 -0500

    Create models and JPA Repositories

--- fundamentos/src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ fundamentos/src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -4,13 +4,20 @@ import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentos.springboot.fundamentos.entity.User;
 import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
+import com.fundamentos.springboot.fundamentos.repository.UserRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
@@ -31,6 +38,8 @@ public class FundamentosApplication implements CommandLineRunner {
 
     private UserPojo userPojo;
 
+    private UserRepository userRepository;
+
     // =====================================
     // Constructor
     // =====================================
@@ -44,7 +53,8 @@ public class FundamentosApplication implements CommandLineRunner {
             MyBean myBean,
             MyBeanWithDependency myBeanWithDependency,
             MyBeanWithProperties myBeanWithProperties,
-            UserPojo userPojo
+            UserPojo userPojo,
+            UserRepository userRepository
     ) {
 
         // Inyeccion de componentes
@@ -53,6 +63,7 @@ public class FundamentosApplication implements CommandLineRunner {
         this.myBeanWithDependency = myBeanWithDependency;
         this.myBeanWithProperties = myBeanWithProperties;
         this.userPojo = userPojo;
+        this.userRepository = userRepository;
     }
 
     // =====================================
@@ -61,6 +72,40 @@ public class FundamentosApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        // Call previous test in the classes
+        this._previousTest();
+
+        // Save users data in data base
+        this._saveUsersInDataBase();
+    }
+
+
+    // =====================================
+    // Private methods
+    // =====================================
+
+    private void _saveUsersInDataBase() {
+        User user1 = new User("Sebastian", "jsvargasq@hotmail.com", LocalDate.of(2001, 3, 10));
+        User user2 = new User("Juan", "jsvargasq@unbosque.edu.co", LocalDate.of(2021, 6, 22));
+        User user3 = new User("User3", "correo3@hotmail.com", LocalDate.of(2021, 3, 23));
+        User user4 = new User("User4", "correo4@hotmail.com", LocalDate.of(2021, 4, 24));
+        User user5 = new User("User5", "correo5@hotmail.com", LocalDate.of(2021, 5, 25));
+        User user6 = new User("User6", "correo6@hotmail.com", LocalDate.of(2021, 6, 26));
+        User user7 = new User("User7", "correo7@hotmail.com", LocalDate.of(2021, 7, 27));
+        User user8 = new User("User8", "correo8@hotmail.com", LocalDate.of(2021, 8, 28));
+        User user9 = new User("User9", "correo9@hotmail.com", LocalDate.of(2021, 9, 29));
+        User user10 = new User("User10", "correo10@hotmail.com", LocalDate.of(2021, 10, 10));
+        User user11 = new User("User11", "correo11@hotmail.com", LocalDate.of(2021, 11, 11));
+        User user12 = new User("User12", "correo12@hotmail.com", LocalDate.of(2021, 12, 12));
+
+        // Create array with all users
+        List<User> users = Arrays.asList(user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11, user12);
+
+        // Save each user of list
+        users.stream().forEach( userRepository::save );
+    }
+
+    private void _previousTest() {
         // Call of the 'saludar' method
         componentDependency.saludar();
 
@@ -73,17 +118,15 @@ public class FundamentosApplication implements CommandLineRunner {
         // Call my bean with properties
         System.out.println(myBeanWithProperties.fuction());
 
-        System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword() + " _ Age: " + userPojo.getAge() );
+        System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword() + " _ Age: " + userPojo.getAge());
 
         // Example of Error Log
         try {
             int value = 10 / 0;
             LOGGER.debug("Response of operation in Try - Catch is: " + value);
-        } catch ( Exception e ) {
+        } catch (Exception e) {
             LOGGER.error("Esto es un error en Try - Catch: " + e.getMessage());
         }
-
-
     }
 
     // =====================================
---
+++ fundamentos/src/main/java/com/fundamentos/springboot/fundamentos/entity/Post.java
@@ -0,0 +1,79 @@
+package com.fundamentos.springboot.fundamentos.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post")
+public class Post {
+
+    // =====================================
+    // Atributtes
+    // =====================================
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_post", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @ManyToOne
+    private User user;
+
+    // =====================================
+    // Constructor
+    // =====================================
+
+    public Post() {
+
+    }
+
+    public Post(String description, User user) {
+        this.description = description;
+        this.user = user;
+    }
+
+
+    // =====================================
+    // Public methods
+    // =====================================
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", user=" + user +
+                '}';
+    }
+
+
+    // =====================================
+    // Getters & setters
+    // =====================================
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
